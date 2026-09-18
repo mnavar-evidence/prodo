39624c11fae831b6e765cc95239058d88f7efc62
mnavar-evidence <mnavar@gmail.com>
2026-09-17 23:19:37 -0700

    Add founder contact email

--- site-config.js
+++ site-config.js
@@ -1,6 +1,6 @@
 /* Public information only. Never put secrets or API keys in this file. */
 window.PRODO_CONFIG = Object.freeze({
-  email: "",
+  email: "mnavar@evidencevc.com",
   founderBio: "",
   founderProfileUrl: "https://www.linkedin.com/in/murgeshnavar/"
 });
